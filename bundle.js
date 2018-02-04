f2a794633342250c77fa97bb95398ad2ffa2fb2a
Travis CI <gabriel@garox.org>
2018-02-04 17:32:16 +0000

    Deploy to GitHub Pages: f5958d598dbc2a18e24d98299af06173f6864f20

--- bundle.js
+++ bundle.js
@@ -47,7 +47,7 @@ var peers = []
 var peerId = new Buffer(hat(160), 'hex')
 
 var torrentData = {}
-
+window.states = { torrentData, peers, peerId, state, currentPathId }
 // create canvas
 var canvas = document.createElement('canvas')
 var ctx = canvas.getContext('2d')
